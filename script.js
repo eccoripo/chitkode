e87d35b24027ad2c078d5a222b29b7cfcff205ef
Emin Bayrak <eminb@outlook.com>
2025-08-17 15:11:21 -0400

    fix: Remove outdated download links from index.html and update GitHub download link in script.js to version 0.0.23

--- script.js
+++ script.js
@@ -165,9 +165,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Download logic
             if (platform === 'windows-github') {
-                window.open('https://github.com/eccoripo/chitkode/releases/download/release.0.0.22/ChitKode.Setup.0.0.22.exe', '_blank');
-            } else if (platform === 'windows-drive') {
-                window.open('https://drive.google.com/file/d/17BgMF9rKNqi6CpNvDPk1RxGw8xjEPE6i/view?usp=sharing', '_blank');
+                window.open('https://github.com/eccoripo/chitkode/releases/download/release.0.0.23/ChitKode.0.0.23.exe', '_blank');
             }
         });
     });
